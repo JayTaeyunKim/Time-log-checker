8c7a296526b35fa92914e8e7b24fef49d9b6cf05
Jay Kim <jay7491@naver.com>
2022-01-23 21:12:21 +0900

    second commit

--- time_log.py
+++ time_log.py
@@ -1,20 +1,33 @@
 from datetime import datetime
-import csv
 import pandas as pd
+import os.path
 
+#파일명 생성
 today = datetime.today()
 filedir = today.strftime("%Y.%m.%d")+'.csv'
 
-start = []
-stop = []
-mins = []
+#파일존재 확인
+if os.path.exists(filedir) == True:
+    timelog = pd.read_csv(filedir)
+    timelog = timelog.iloc[:-1, :] #마지막열에 sum을 기록하기 때문에 삭제
+    start = []
+    stop = []
+    mins = []
+else:
+    start = []
+    stop = []
+    mins = []
+    timelog = pd.DataFrame(data={'start': start, 'stop': stop, 'total': mins})
 
 flag = ' '
-for i in range(9999):
-    flag = input("press any key to start...")
-
+for i in range(99999):
+    #exit입력시 종료
     if flag == "exit":
         break
+    else:
+        flag = input("Press enter to start...")
+        if flag == "exit":
+            break
 
     #시작
     current_time = datetime.now()
@@ -23,27 +36,35 @@ for i in range(9999):
     print("Current Time: ", start[i])
 
     #멈춤
-    flag = input("press any key to stop...")
+    flag = input("Press enter to stop...")
     current_time = datetime.now()
     stop.append(current_time.strftime("%H:%M:%S"))
     minutes = int(round(current_time.timestamp())) - minutes
     print("Current Time: ", stop[i])
 
     #총
-    mins.append(minutes/60)
+    mins.append(round(minutes/60, 2))
     print("Lasted time: ", round(minutes/60, 2))
 
+    print()
+
 #마지막 total 저장
-sum = sum(mins)
+sum = sum(timelog['total'].astype(str).astype(float))+sum(mins) #기존 데이터 타입이 object이기 때문에
+                                                                # float로 변환하여 새로운 기록과 합함
 start.append("")
 stop.append("")
-mins.append('{}h {}m'.format(int(sum/60), int(sum)))
-timelog = pd.DataFrame(data={'start': start, 'stop': stop, 'total': mins})
+mins.append('{}h {}m'.format(int(sum/60), int(sum%60)))
+
+timelog = pd.concat([timelog,
+                     pd.DataFrame(data={'start': start, 'stop': stop, 'total': mins})],
+                    axis=0, ignore_index=True)
 
 print(timelog)
-print('total: {}h {}m'.format(int(sum/60), int(sum)))
+print('total: {}h {}m'.format(int(sum/60), int(sum%60)))
 
 #파일로 저장
-timelog.to_csv(filedir, mode='a', index=False, encoding='utf8')
+timelog.to_csv(filedir, mode='w', index=False, encoding='utf8')
+
+input() #화면 꺼짐 방지용
 
 
